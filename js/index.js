b606c43fd885c21b676935329505567ad1119541
GitStudentSem <programmSem@yandex.ru>
2021-04-02 15:41:06 +0300

    main task 2 lesson

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 45999;
+let income = "премия";
+let addExpenses = "Квартира, Еда, Коммунальные платежи";
+let deposit = true;
+let mission = 100000;
+let period = 6;
 
-alert("Сообщение Алерт");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+console.log(addExpenses.toLowerCase().split(", "));
 
-console.log("Сообщение в консоль");
+let budgetDay = money / 30;
+console.log("budgetDay: ", budgetDay);
