032b02ba4123a819677487ccc4bcacb237cee197
GitStudentSem <programmSem@yandex.ru>
2021-04-09 18:35:36 +0300

    lesson06

---
+++ js/lesson06.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function hideNumber() {
+  let secretNumber = 38; // замкнуто секретное число
+
+  // рекурсия
+  return function checkingIsNumber() {
+    let interNumber = prompt("Угадай число от 1 до 100");
+    // если введенное число - не число ввести снова
+    if (isNaN(parseFloat(interNumber))) {
+      alert("Введи число!");
+      checkingIsNumber();
+    } else {
+      // если введено число, то проверяем его на больше/меньше/равно
+      if (interNumber !== null) {
+        // проверка на отмену
+        if (interNumber > secretNumber) {
+          alert("Загаданное число меньше");
+          checkingIsNumber();
+        } else if (interNumber < secretNumber) {
+          alert("Загаданное число больше");
+          checkingIsNumber();
+        } else {
+          alert("Поздравляю, Вы угадали!!!");
+        }
+      }
+    }
+  };
+}
+
+let play = hideNumber(); // check это функция checkingIsNumber
+console.log(play());
