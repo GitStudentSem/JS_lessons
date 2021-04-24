8be1286a5f7e1d2b630a29436e066f4a86474257
GitStudentSem <programmSem@yandex.ru>
2021-04-24 13:49:23 +0300

    pause

---
+++ newYear/js/lesson18.2.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+  const hello = document.createElement("div");
+  const dayOfWeek = document.createElement("div");
+  const timerDay = document.createElement("div");
+  const timerHours = document.createElement("div");
+  const timerMinutes = document.createElement("div");
+  const timerSeconds = document.createElement("div");
+  document.body.append(hello);
+  document.body.append(dayOfWeek);
+  document.body.append(timerDay);
+  document.body.append(timerHours);
+  document.body.append(timerMinutes);
+  document.body.append(timerSeconds);
+});
