b9b6235b6049533bbf2df74b033cdad44530ebe3
GitStudentSem <programmSem@yandex.ru>
2021-04-25 15:27:07 +0300

    add old work

--- js/lesson18.js
+++ js/lesson18.js
@@ -1,8 +1,8 @@
 window.addEventListener("DOMContentLoaded", function () {
   "use strict";
 
-  //Таймер
-  function countTimer(deadline) {
+  // Таймер
+  const countTimer = (deadline) => {
     let timerHours = document.querySelector("#timer-hours");
     let timerMinutes = document.querySelector("#timer-minutes");
     let timerSeconds = document.querySelector("#timer-seconds");
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function () {
     let colonFirst = document.querySelectorAll(".timer-numbers > span")[1];
     let colonSecond = document.querySelectorAll(".timer-numbers > span")[3];
 
-    function getTimeRemaining() {
+    const getTimeRemaining = () => {
       // getTime переводит значения в милисекунды
       let dateStop = new Date(deadline).getTime();
       let dateNow = new Date().getTime();
@@ -22,29 +22,35 @@ window.addEventListener("DOMContentLoaded", function () {
       let minutes = Math.floor((timeRemaining / 60) % 60);
       let hours = Math.floor(timeRemaining / 60 / 60);
       return { timeRemaining, hours, minutes, seconds };
-    }
+    };
 
-    function updateClock() {
-      let timer = getTimeRemaining();
-
-      // Вывод значений на странцу
-      function formatTime(time) {
-        if (time < 10) {
-          return "0" + time;
-        } else {
-          return time;
-        }
+    // Добавление 0 к одной цифре
+    const formatTime = (time) => {
+      if (time < 10) {
+        return "0" + time;
+      } else {
+        return time;
       }
+    };
+
+    // Время для первой секунды, одноразовая отрисока значений
+    const firstSecond = () => {
+      let timer = getTimeRemaining();
+      // Вывод значений на странцу;
+      timerHours.textContent = formatTime(timer.hours);
+      timerMinutes.textContent = formatTime(timer.minutes);
+      timerSeconds.textContent = formatTime(timer.seconds);
+    };
+    firstSecond();
+
+    let interval = setInterval(() => {
+      let timer = getTimeRemaining();
+      // Вывод значений на странцу
       timerHours.textContent = formatTime(timer.hours);
       timerMinutes.textContent = formatTime(timer.minutes);
       timerSeconds.textContent = formatTime(timer.seconds);
 
-      // Переменная объявлена вне условия, что бы она было видна в блоке else
-      let interval;
-      if (timer.timeRemaining > 0) {
-        interval = setInterval(updateClock, 1000);
-        console.log("interval: ", interval);
-      } else {
+      if (timer.timeRemaining < 0) {
         clearInterval(interval);
         //Кросный текст
         timerHours.style.color = "red";
@@ -58,8 +64,28 @@ window.addEventListener("DOMContentLoaded", function () {
         timerMinutes.textContent = "00";
         timerSeconds.textContent = "00";
       }
-    }
-    updateClock();
-  }
+    }, 1000);
+  };
   countTimer("27 april 2021");
+
+  // Меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector(".menu");
+    const menu = document.querySelector("menu");
+    const closeBtn = document.querySelector(".close-btn");
+    const menuItems = menu.querySelectorAll("ul > li");
+
+    const handlerMenu = () => {
+      menu.classList.toggle("active-menu");
+    };
+
+    btnMenu.addEventListener("click", handlerMenu);
+
+    closeBtn.addEventListener("click", handlerMenu);
+
+    menuItems.forEach((elem) => {
+      elem.addEventListener("click", handlerMenu);
+    });
+  };
+  toggleMenu();
 });
