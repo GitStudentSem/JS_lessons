30968dfd05347ef752b46b9e67ab1eb6217e930d
GitStudentSem <programmSem@yandex.ru>
2021-04-27 11:36:39 +0300

    check 2

--- js/lesson17.2.js
+++ js/lesson17.2.js
@@ -13,3 +13,64 @@ const cityArr = {
   bel: ["Минск", "Гомель", "Могилёв", "Витебск", "Гродно", "Брест"],
   jap: ["Токио", "Киото", "Осака", "Иокогама"],
 };
+let country = document.querySelector("#country");
+let city = document.querySelector("#city");
+let result = document.querySelector(".result");
+
+country.addEventListener("input", function () {
+  let cityes;
+  if (country.value === "rus") {
+    cityes = cityArr[country.value];
+
+    city.innerHTML += `
+    <select>`;
+
+    cityes.forEach((cityRus) => {
+      city.innerHTML += `
+      <option value="rus">${cityRus}</option>`;
+    });
+
+    city.innerHTML += `
+    </select>`;
+  } else if (country.value === "uk") {
+    cityes = cityArr[country.value];
+
+    city.innerHTML += `
+    <select>`;
+
+    cityes.forEach((cityUk) => {
+      city.innerHTML += `
+      <option value="rus">${cityUk}</option>`;
+    });
+
+    city.innerHTML += `
+    </select>`;
+  } else if (country.value === "bel") {
+    cityes = cityArr[country.value];
+
+    city.innerHTML += `
+    <select>`;
+
+    cityes.forEach((cityBel) => {
+      city.innerHTML += `
+      <option value="rus">${cityBel}</option>`;
+    });
+
+    city.innerHTML += `
+    </select>`;
+  } else if (country.value === "jap") {
+    cityes = cityArr[country.value];
+
+    city.innerHTML += `
+    <select>`;
+    cityes.forEach((cityJap) => {
+      city.innerHTML += `
+      <option value="rus">${cityJap}</option>`;
+    });
+
+    city.innerHTML += `
+    </select>`;
+  }
+});
+
+// console.log(spisok);
