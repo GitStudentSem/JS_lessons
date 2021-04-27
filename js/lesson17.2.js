06393ee6fd4b32026ddfa2e030e2e7a85225ca04
GitStudentSem <programmSem@yandex.ru>
2021-04-27 16:37:06 +0300

    create new functions

--- js/lesson17.2.js
+++ js/lesson17.2.js
@@ -1,48 +1,4 @@
 "use strict";
-// const cityArr = {
-//     rus: [
-//       "Москва",
-//       "Санк-Петербург",
-//       "Новосибирск",
-//       "Екатеринбург",
-//       "Нижний Новгород",
-//       "Казань",
-//       "Челябинск",
-//     ],
-//     uk: ["Киев", "Харьков", "Одесса", "Днепр", "Донецк", "Запорожье", "Львов"],
-//     bel: ["Минск", "Гомель", "Могилёв", "Витебск", "Гродно", "Брест"],
-//     jap: ["Токио", "Киото", "Осака", "Иокогама"],
-//   },
-//   countryArr = { rus: "Россия", uk: "Украина", bel: "Беларусь", jap: "Япония" },
-//   countrySelect = document.querySelector("#country"),
-//   citySelect = document.querySelector("#city"),
-//   resultPlace = document.querySelector(".result");
-
-// countrySelect.addEventListener("input", () => {
-//   bildSitySelect(cityArr[countrySelect.value]);
-// });
-
-// const bildSitySelect = (sityList = cityArr.rus) => {
-//   // значение по умолчанию т.к. я вызываю функцию еще до выбора пользователя
-//   [...citySelect].forEach((option) => option.remove());
-
-//   //очистка select перед рендером новых значений. Оператор spead позволяет использовать forEach удля элемента citySelect
-
-//   sityList.forEach((sityName) => {
-//     let option = document.createElement("option");
-//     option.value, (option.textContent = sityName);
-//     citySelect.prepend(option);
-//   });
-// };
-
-// citySelect.addEventListener("click", () => {
-//   resultPlace.textContent = `${countryArr[countrySelect.value]}, ${
-//     citySelect.value
-//   }`;
-// });
-
-// bildSitySelect();
-////////////////////////
 const cityArr = {
   rus: [
     "Москва",
@@ -67,15 +23,14 @@ let country = document.querySelector("#country");
 let city = document.querySelector("#city");
 let result = document.querySelector(".result");
 
-const start = function (cityes = cityArr.rus) {
-  cityes = cityArr[country.value];
-  let countrys = coutryArr[country.value];
-
+const remove = () => {
   // Удаление
   while (city.firstChild) {
     city.removeChild(city.firstChild);
   }
+};
 
+const add = (cityes) => {
   // Добавление
   cityes.forEach((elem) => {
     let option = document.createElement("option");
@@ -83,11 +38,22 @@ const start = function (cityes = cityArr.rus) {
     option.textContent = elem;
     city.prepend(option);
   });
+};
 
+const inner = (countrys) => {
   //Вывод на страницу
   city.addEventListener("input", () => {
     result.innerHTML = `${countrys}, ${city.value}`;
   });
+};
+
+const start = (cityes = cityArr.rus) => {
+  cityes = cityArr[country.value];
+  let countrys = coutryArr[country.value];
+
+  remove();
+  add(cityes);
+  inner(countrys);
 };
 start();
 
