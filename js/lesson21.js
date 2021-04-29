d06591f76b1c36e29c2fce6823045519540fd37f
GitStudentSem <programmSem@yandex.ru>
2021-04-29 23:14:48 +0300

    fix validator

--- js/lesson21.js
+++ js/lesson21.js
@@ -318,14 +318,8 @@ window.addEventListener("DOMContentLoaded", function () {
     photos.forEach((photo) => {
       photo.addEventListener("mouseenter", (event) => {
         srcReg = event.target.src;
+        console.log("srcReg: ", srcReg);
         event.target.src = event.target.dataset.img;
-
-        /* Обрезка от лишних символов в пути к картинке
-        Искать вождение с /images, любое количество символов {0,}
-        До jpg флаг g ищет всю строку, а не только первое вхождение*/
-        let result = srcReg.match(/\/images.{0,}/g);
-        /* заменяю полное вхождение на обрезанное и присваиваю в ту же переменную, тем самы перезаписав её*/
-        srcReg = srcReg.replace(srcReg, result);
       });
 
       photo.addEventListener("mouseleave", (event) => {
@@ -355,9 +349,11 @@ window.addEventListener("DOMContentLoaded", function () {
     const phone = document.getElementById("form2-phone");
     const message = document.getElementById("form2-message");
 
-    name.addEventListener("blur", () => {
+    name.addEventListener("input", () => {
       // [^а-яё\-\s] Обрезает все символы кроме русских букв тире и пробел
-      name.value = name.value.replace(/[^а-яё\-\s]/gi, "");
+      name.value = name.value.replace(/[^а-яё-\s]/gi, "");
+    });
+    name.addEventListener("blur", () => {
       // Заменяет 2 и более тире на один
       name.value = name.value.replace(/-{1,}/g, "-");
       // Заменяет 2 и более пробела на один
@@ -365,24 +361,29 @@ window.addEventListener("DOMContentLoaded", function () {
       // Удаляет пробелы и тире в начале и конце строки
       name.value = name.value.replace(/^\s|\s$|^-|-$/g, "");
       // первая буква большая, остальные маленькие
-      // Массив для хранения отдельных слов
-      let nameFirstBig = [];
-      // Бью строку по пробелу и создаю массив из слов
-      let word = name.value.split(" ");
-      word.forEach((item) => {
-        // Каждый элемент массива привожу к большой букве
-        item = item[0].toUpperCase() + item.slice(1);
-        // отправляю элементы в пустой массив на хранение
-        nameFirstBig.push(item);
-      });
+      // ЗАщита от пустой строки, что бы консоль не ругалась
+      if (name.value !== "") {
+        // Массив для хранения отдельных слов
+        let nameFirstBig = [];
+        // Бью строку по пробелу и создаю массив из слов
+        let word = name.value.split(" ");
+        word.forEach((item) => {
+          // Каждый элемент массива привожу к большой букве
+          item = item[0].toUpperCase() + item.slice(1);
+          // отправляю элементы в пустой массив на хранение
+          nameFirstBig.push(item);
+        });
+        // Делаю из массива строку и отправляю её в значение поля инпут
+        name.value = nameFirstBig.join(" ");
+      }
+    });
 
-      // Делаю из массива строку и отправляю её в значение поля инпут
-      name.value = nameFirstBig.join(" ");
+    email.addEventListener("input", () => {
+      // [^a-z@-_.!~*'] Обрезает все символы кроме указанных
+      email.value = email.value.replace(/[^a-z@-_.!~'*]/g, "");
     });
 
     email.addEventListener("blur", () => {
-      // [^a-z@-_.!~*'] Обрезает все символы кроме указанных
-      email.value = email.value.replace(/[^[a-z]@-_.!~*']/gi, "");
       // Обрезает 2 и более тире
       email.value = email.value.replace(/-{1,}/g, "-");
       // Удаляет тире в начале и конце строки
@@ -392,10 +393,17 @@ window.addEventListener("DOMContentLoaded", function () {
     phone.addEventListener("blur", () => {
       // [^0-9] Обрезает все символы кроме цифр
       phone.value = phone.value.replace(/[^0-9()-]/g, "");
+    });
+
+    phone.addEventListener("input", () => {
       // Обрезает 2 и более тире
       phone.value = phone.value.replace(/-{1,}/g, "-");
       // Удаляет тире в начале и конце строки
       phone.value = phone.value.replace(/^-|-$/g, "");
+    });
+
+    message.addEventListener("input", () => {
+      message.value = message.value.replace(/[^а-яё-\s]/gi, "");
     });
   };
   footerForm();
